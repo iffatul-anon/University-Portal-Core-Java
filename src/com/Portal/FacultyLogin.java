693005dbfdce6c860490af69c318ea98e414510d
Md. Iffatul Islam Anon <anon35-1065@diu.edu.bd>
2023-11-19 16:07:14 +0600

    Initial Commit

---
+++ src/com/Portal/FacultyLogin.java
@@ -0,0 +1,3 @@
+package com.Portal;
+public class FacultyLogin {
+}
---
+++ src/com/Portal/Main.java
@@ -0,0 +1,11 @@
+package com.Portal;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            StudentLogin.studentLogin();
+        }
+    }
+}
